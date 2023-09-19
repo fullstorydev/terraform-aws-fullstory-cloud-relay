fc1eeae972a4b935f1341729087acda98028d32a
Ian Rosenbery <iamrosenbery@gmail.com>
2023-09-19 15:40:12 -0700

    Add optional variable to define cloudfront origin request policy name

--- variables.tf
+++ variables.tf
@@ -20,3 +20,9 @@ variable "acm_certificate_arn" {
   description = "(optional) The ARN of the ACM certificate to be used for the relay. If omitted, a value for `route53_zone_name` must be provided. Defaults to null."
   default     = null
 }
+
+variable "cloudfront_origin_request_policy_name" {
+  type        = string
+  description = "(optional) A name to uniquely identify the cloudfront origin request policy for the relay. This is required to deploy multiple relay modules to the same AWS account, as such policies must be uniquely named. Example: `fullstory-relay-origin-request-policy-production`."
+  default     = "fullstory-relay-origin-request-policy"
+}
